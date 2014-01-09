3057bd7597f1cc517b02326e5fc5db822f755259
Vince Zhang <vzhang@crosscap.com>
2014-01-09 10:37:19 -0500

    Initialize Projects

---
+++ cd-parent/cd-dao/src/main/java/com/vince/cd/cd_dao/App.java
@@ -0,0 +1,13 @@
+package com.vince.cd.cd_dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cd-parent/cd-service/src/main/java/com/vince/cd/App.java
@@ -0,0 +1,13 @@
+package com.vince.cd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
